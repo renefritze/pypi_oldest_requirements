60efefe8d0ec98f58aa8ebdf6bfa0f67d60dfefc
René Fritze <rene.fritze@wwu.de>
2019-03-18 11:32:04 +0100

    [ci] extend tests with more requirements

--- tests/test_pypi_oldest_requirements.py
+++ tests/test_pypi_oldest_requirements.py
@@ -10,11 +10,12 @@ import pytest
 from pypi_oldest_requirements import req_parse
 
 
-def test_command_line_interface():
+@pytest.mark.parametrize("req_name", ['requirements.txt', 'requirements-optional.txt'])
+def test_command_line_interface(req_name):
     """Test the CLI."""
     this_dir = os.path.dirname(os.path.abspath(__file__))
-    req_file = os.path.join(this_dir, 'requirements.txt')
-    pckl = os.path.join(this_dir, 'result.pickle')
+    req_file = os.path.join(this_dir, req_name)
+    pckl = os.path.join(this_dir, f'{req_name}.pickle')
     res = {(n, str(o)) for n, o in req_parse.get_oldest_from_req_file(req_file)}
     pickle.dump(res, open(pckl+'.new', 'wb'))
     assert res == pickle.load(open(pckl, 'rb'))
