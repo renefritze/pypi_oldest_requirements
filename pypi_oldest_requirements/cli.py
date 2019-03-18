69ff55e41830bbfd3b91da3e630186d26994349b
René Fritze <rene.fritze@wwu.de>
2019-03-18 10:28:36 +0100

    add fallback parsing

--- pypi_oldest_requirements/cli.py
+++ pypi_oldest_requirements/cli.py
@@ -5,8 +5,8 @@ import sys
 
 
 def main(requirements_file):
-    from pypi_oldest_requirements import requirements
-    for name, oldest in requirements.get_oldest_from_req_file(requirements_file):
+    from pypi_oldest_requirements import req_parse
+    for name, oldest in req_parse.get_oldest_from_req_file(requirements_file):
         print(f'{name}: {oldest}')
 
 
--- pypi_oldest_requirements/req_parse.py
+++ pypi_oldest_requirements/req_parse.py
@@ -1,12 +1,17 @@
-from . import pypi
-
 from pkg_resources import parse_requirements
+from requirements import parser
+from packaging.specifiers import SpecifierSet
 
+from pypi_oldest_requirements import pypi
 
 def get_oldest_version(req):
     available_versions = pypi.get_versions(req.name)
     # actually a packaging.specifiers.SpecifierSet
-    spec_set = req.specifier
+    if isinstance(req.specifier, SpecifierSet):
+        spec_set = req.specifier
+    else:
+        spec_set = SpecifierSet(specifiers=','.join((''.join(s) for s in req.specs)))
+
     for av in sorted(available_versions, reverse=False):
         # for some reason 'av' is incompatible 'Version" object that spec_set wants to parse again
         if spec_set.contains(str(av)):
@@ -15,6 +20,13 @@ def get_oldest_version(req):
 
 
 def get_oldest_from_req_file(req_file):
-    parsed = parse_requirements(open(req_file, 'rt').readlines())
+    parsed = []
+    for line in open(req_file, 'rt').readlines():
+        try:
+            gen = list(parse_requirements(line))
+        except :
+            gen = list(parser.parse(line))
+        for req in gen:
+            parsed.append(req)
     for req in parsed:
         yield req.name, get_oldest_version(req)
