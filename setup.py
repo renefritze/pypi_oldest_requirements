fa6753bf80e0408c68a6699d97ab851c984fdf22
René Fritze <rene.fritze@wwu.de>
2020-01-20 14:15:29 +0100

    bump version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/renefritze/pypi_oldest_requirements',
-    version='2019.5',
+    version='2020.2',
     zip_safe=False,
     python_requires='>=3.6',
 )
