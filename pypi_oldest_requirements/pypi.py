5cb3e3607d1647c052af2110c1b25acf81f0bc57
René Fritze <rene.fritze@wwu.de>
2020-01-20 14:09:12 +0100

    force tls 1.2 in pypi api communication

--- pypi_oldest_requirements/pypi.py
+++ pypi_oldest_requirements/pypi.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
-
+import ssl
 import requests
 from packaging import version
+from requests_toolbelt import SSLAdapter
 
 
 def get_versions(package_name):
-    r = requests.get(f"https://pypi.org/pypi/{package_name}/json")
+    url = f"https://pypi.org/pypi/{package_name}/json"
+    s = requests.Session()
+    s.mount('https://', SSLAdapter(ssl.PROTOCOL_TLSv1_2))
+    r = s.get(url)
     try:
         versions = [version.parse(k) for k in r.json()['releases'].keys()]
     except KeyError:
