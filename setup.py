f61e1c17f33e99a759656990f510769acb99c03d
René Fritze <rene.fritze@wwu.de>
2021-05-18 15:32:47 +0200

    bump supported pythons

--- setup.py
+++ setup.py
@@ -19,8 +19,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description='Get "oldest" fitting requirements from pypi',
     entry_points={
