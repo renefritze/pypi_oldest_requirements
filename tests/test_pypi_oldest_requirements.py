4d8e06b610e43ed6e733f6232c0ad0307ed5f1d4
René Fritze <rene.fritze@wwu.de>
2021-05-19 10:25:07 +0200

    [ci] switch to pytest-regeressions

--- tests/test_pypi_oldest_requirements.py
+++ tests/test_pypi_oldest_requirements.py
@@ -2,38 +2,27 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `pypi_oldest_requirements` package."""
-import os
-import pickle
 import sys
 import pytest
 
 from pypi_oldest_requirements import req_parse
 
 
-@pytest.mark.parametrize("req_name", ['requirements.txt', 'requirements-optional.txt'])
-def test_oldest_req(req_name):
-    """Test the CLI."""
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    req_file = os.path.join(this_dir, req_name)
-    pckl = os.path.join(this_dir, f'{req_name}_{sys.version_info.major}.{sys.version_info.minor}.pickle')
-    res = {(n, str(o)) for n, o in req_parse.get_oldest_from_req_file(req_file)}
-    pickle.dump(res, open(pckl+'.new', 'wb'))
-    control = pickle.load(open(pckl, 'rb'))
-    assert res == control
+@pytest.fixture(params=['requirements.txt', 'requirements-optional.txt'])
+def requirement_file(request, shared_datadir):
+    fn = shared_datadir / request.param
+    assert fn.exists()
+    return fn
 
 
-@pytest.mark.parametrize("req_name", ['requirements.txt', 'requirements-optional.txt'])
-def test_minimal_req(req_name):
-    """Test the CLI."""
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    req_file = os.path.join(this_dir, req_name)
-    pckl = os.path.join(this_dir, f'{req_name}_{sys.version_info.major}.{sys.version_info.minor}.minimal.pickle')
-    res = list(req_parse.get_minimal_restricted_from_req_file(req_file))
-    pickle.dump(res, open(pckl+'.new', 'wb'))
-    control = pickle.load(open(pckl, 'rb'))
-    ldiff = set(res) - set(control)
-    rdiff = set(control) - set(res)
-    assert ldiff == rdiff
+def test_oldest_req(requirement_file, data_regression):
+    res = {(n, str(o)) for n, o in req_parse.get_oldest_from_req_file(requirement_file)}
+    data_regression.check(res)
+
+
+def test_minimal_req(requirement_file, file_regression):
+    res = ''.join(req_parse.get_minimal_restricted_from_req_file(requirement_file))
+    file_regression.check(res)
 
 
 if __name__ == '__main__':
