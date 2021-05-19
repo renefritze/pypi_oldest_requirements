b9e8acac9a66c61a1f863275d49cd05c01a258bb
René Fritze <rene.fritze@wwu.de>
2021-05-19 15:11:54 +0200

    test multi file input

--- tests/test_pypi_oldest_requirements.py
+++ tests/test_pypi_oldest_requirements.py
@@ -8,20 +8,20 @@ import pytest
 from pypi_oldest_requirements import req_parse
 
 
-@pytest.fixture(params=['requirements.txt', 'requirements-optional.txt'])
-def requirement_file(request, shared_datadir):
-    fn = shared_datadir / request.param
-    assert fn.exists()
+@pytest.fixture(params=[['requirements.txt'], ['requirements-optional.txt'], ['requirements.txt', 'requirements-optional.txt']])
+def requirement_files(request, shared_datadir):
+    fn = [shared_datadir / p for p in request.param]
+    assert all(f.exists() for f in fn)
     return fn
 
 
-def test_oldest_req(requirement_file, data_regression):
-    res = {(n, str(o)) for n, o in req_parse.get_oldest_from_req_file(requirement_file)}
+def test_oldest_req(requirement_files, data_regression):
+    res = {(n, str(o)) for n, o in req_parse.get_oldest_from_req_file(requirement_files)}
     data_regression.check(res)
 
 
-def test_minimal_req(requirement_file, file_regression):
-    res = ''.join(req_parse.get_minimal_restricted_from_req_file(requirement_file))
+def test_minimal_req(requirement_files, file_regression):
+    res = ''.join(req_parse.get_minimal_restricted_from_req_file(requirement_files))
     file_regression.check(res)
 
 
